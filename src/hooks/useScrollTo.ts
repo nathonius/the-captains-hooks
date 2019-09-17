fbc4b9d04f06beb50335ff6a4cce74c5e80440a9
Nathan <nathan@praisetopia.org>
2019-09-17 11:24:20 -0500

    Testing useScrollTo and made it default to smooth behavior (#9)
    * add smooth to scrollTo
    
    * added at least one scrollTo test

--- src/hooks/useScrollTo.ts
+++ src/hooks/useScrollTo.ts
@@ -3,10 +3,10 @@ import { useRef, useCallback, Ref, RefObject } from 'react';
 export function useScrollTo<T extends Element>(ref: RefObject<T>): () => void {
   const scrollTo = useCallback(() => {
     if (ref.current) {
-      window.scrollTo(
-        0,
-        ref.current.getBoundingClientRect().top + window.scrollY
-      );
+      window.scrollTo({
+        top: ref.current.getBoundingClientRect().top + window.scrollY,
+        behavior: 'smooth'
+      });
     }
   }, [ref, ref.current]);
   return scrollTo;

8e50f272607d962f3159d3846e10748dd93bd4a7
Nathan Smith <nathan@nathan-smith.org>
2019-09-17 21:35:21 -0500

    remove bad export

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,7 @@
-export { useDebounce } from "./hooks/useDebounce";
-export { useEventListener } from "./hooks/useEventListener";
-export { useHover } from "./hooks/useHover";
-export { useLockBodyScroll } from "./hooks/useLockBodyScroll";
-export { useOnClickOutside } from "./hooks/useOnClickOutside";
-export { useOnScreen } from "./hooks/useOnScreen";
-export { useScrollTo } from "./hooks/useScrollTo";
-export { useWindowSize } from "./hooks/useWindowSize";
+export { useDebounce } from './hooks/useDebounce';
+export { useHover } from './hooks/useHover';
+export { useLockBodyScroll } from './hooks/useLockBodyScroll';
+export { useOnClickOutside } from './hooks/useOnClickOutside';
+export { useOnScreen } from './hooks/useOnScreen';
+export { useScrollTo } from './hooks/useScrollTo';
+export { useWindowSize } from './hooks/useWindowSize';
